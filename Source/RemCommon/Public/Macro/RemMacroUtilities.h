e2610a5ba107f5411d51e939a0ff167f0d1f4efe
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2024-11-07 23:37:31 +0800

    Misc: more auto

--- Source/RemCommon/Public/Macro/RemMacroUtilities.h
+++ Source/RemCommon/Public/Macro/RemMacroUtilities.h
@@ -5,10 +5,10 @@
 #define REM_WITH_DEVELOPMENT_ONLY_CODE !UE_BUILD_SHIPPING
 
 #define REM_INITIALIZE_NET_DEBUG_STRING(Object, VariableName) \
-	const FString VariableName = Rem::GetNetDebugString(Object);
+	const auto VariableName = Rem::GetNetDebugString(Object);
 
 #define REM_INITIALIZE_SOURCE_LOCATION_STRING(VariableName) \
-	const FString VariableName = FString::Format(TEXT("[Function: {0}] [Line: {1}]"), \
+	const auto VariableName = FString::Format(TEXT("[Function: {0}] [Line: {1}]"), \
 	{__func__, __LINE__});
 
 #ifdef REM_WITH_DEVELOPMENT_ONLY_CODE
