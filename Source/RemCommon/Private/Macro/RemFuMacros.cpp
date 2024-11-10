43c278b96ae633536e4fad4bd5097ea7b0811b36
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2024-11-10 15:07:07 +0800

    New: import FuEnsure
    Source: https://github.com/Sixze/FabulousUtility/blob/main/Source/FabulousUtility/Public/FuMacros.h

---
+++ Source/RemCommon/Private/Macro/RemFuMacros.cpp
@@ -0,0 +1,83 @@
+#include "FuMacros.h"
+
+#include "CoreGlobals.h"
+#include "Async/UniqueLock.h"
+#include "Async/WordMutex.h"
+#include "Containers/StaticArray.h"
+#include "HAL/IConsoleManager.h"
+#include "HAL/PlatformTime.h"
+#include "Templates/Function.h"
+
+#if DO_ENSURE && !USING_CODE_ANALYSIS
+
+bool UE_DEBUG_SECTION FuEnsure::Execute(std::atomic<bool>& bExecuted, const bool bEnsureAlways,
+                                        const ANSICHAR* Expression, const TCHAR* StaticMessage, const TCHAR* Format, ...)
+{
+	static const auto* EnsureAlwaysEnabledConsoleVariable{
+		IConsoleManager::Get().FindConsoleVariable(TEXT("core.EnsureAlwaysEnabled"))
+	};
+	check(EnsureAlwaysEnabledConsoleVariable != nullptr)
+
+	if ((bExecuted.load(std::memory_order_relaxed) &&
+	     (!bEnsureAlways || !EnsureAlwaysEnabledConsoleVariable->GetBool())) ||
+	    !FPlatformMisc::IsEnsureAllowed())
+	{
+		return false;
+	}
+
+	if (bExecuted.exchange(true, std::memory_order_release) && !bEnsureAlways)
+	{
+		return false;
+	}
+
+	static UE::FWordMutex FormatMutex;
+	static constexpr auto FormattedMessageSize{65535};
+	static TStaticArray<TCHAR, FormattedMessageSize> FormattedMessage;
+
+	// ReSharper disable once CppLocalVariableWithNonTrivialDtorIsNeverUsed
+	UE::TUniqueLock Lock{FormatMutex};
+
+	GET_TYPED_VARARGS(TCHAR, FormattedMessage.GetData(), FormattedMessageSize, FormattedMessageSize - 1, Format, Format);
+
+	if (UNLIKELY(GetEnsureHandler() && GetEnsureHandler()({Expression, FormattedMessage.GetData()})))
+	{
+		return false;
+	}
+
+	if (FPlatformTime::GetSecondsPerCycle() != 0.0)
+	{
+		static const auto* EnsuresAreErrorsConsoleVariable{
+			IConsoleManager::Get().FindConsoleVariable(TEXT("core.EnsuresAreErrors"))
+		};
+		check(EnsuresAreErrorsConsoleVariable != nullptr)
+
+#if !NO_LOGGING
+		if (EnsuresAreErrorsConsoleVariable->GetBool())
+		{
+			UE_LOG(LogOutputDevice, Error, TEXT("%s"), StaticMessage);
+			UE_LOG(LogOutputDevice, Error, TEXT("%s"), FormattedMessage.GetData());
+		}
+		else
+		{
+			UE_LOG(LogOutputDevice, Warning, TEXT("%s"), StaticMessage);
+			UE_LOG(LogOutputDevice, Warning, TEXT("%s"), FormattedMessage.GetData());
+		}
+#endif
+
+		PrintScriptCallstack();
+	}
+
+	if (!FPlatformMisc::IsDebuggerPresent())
+	{
+		FPlatformMisc::PromptForRemoteDebugging(true);
+		return false;
+	}
+
+#if UE_BUILD_SHIPPING
+	return true;
+#else
+	return !GIgnoreDebugger;
+#endif
+}
+
+#endif
---
+++ Source/RemCommon/Public/Macro/RemFuMacros.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+
+#include "Misc/AssertionMacros.h"
+
+#define FU_STRINGIFY_IMPLEMENTATION(Value) #Value
+
+#define FU_STRINGIFY(Value) FU_STRINGIFY_IMPLEMENTATION(Value)
+
+#define FU_GET_TYPE_STRING(Type) \
+	((void) sizeof UEAsserts_Private::GetMemberNameCheckedJunk(static_cast<Type*>(nullptr)), TEXTVIEW(#Type))
+
+// A lightweight version of the ensure() macro that doesn't generate a C++ call stack and doesn't send a
+// crash report, because it doesn't happen instantly and causes the editor to freeze, which can be annoying.
+
+#if DO_ENSURE && !USING_CODE_ANALYSIS
+
+namespace FuEnsure
+{
+	FABULOUSUTILITY_API bool UE_DEBUG_SECTION VARARGS Execute(std::atomic<bool>& bExecuted, bool bEnsureAlways, const ANSICHAR* Expression,
+	                                                          const TCHAR* StaticMessage, const TCHAR* Format, ...);
+}
+
+#define FU_ENSURE_IMPLEMENTATION(Capture, bEnsureAlways, Expression, Format, ...) \
+	(LIKELY(Expression) || [Capture]() UE_DEBUG_SECTION \
+	{ \
+		static constexpr auto StaticMessage{TEXT("Ensure failed: " #Expression ", File: " __FILE__ ", Line: " FU_STRINGIFY(__LINE__) ".")}; \
+ 		static std::atomic<bool> bExecuted{false}; \
+ 		\
+		UE_VALIDATE_FORMAT_STRING(Format, ##__VA_ARGS__); \
+		\
+		if (FuEnsure::Execute(bExecuted, bEnsureAlways, #Expression, StaticMessage, Format, ##__VA_ARGS__)) \
+		{ \
+			PLATFORM_BREAK(); \
+		} \
+		\
+		return false; \
+	}())
+
+#define FU_ENSURE(Expression) FU_ENSURE_IMPLEMENTATION( , false, Expression, TEXT(""))
+#define FU_ENSURE_MESSAGE(Expression, Format, ...) FU_ENSURE_IMPLEMENTATION(&, false, Expression, Format, ##__VA_ARGS__)
+#define FU_ENSURE_ALWAYS(Expression) FU_ENSURE_IMPLEMENTATION( , true, Expression, TEXT(""))
+#define FU_ENSURE_ALWAYS_MESSAGE(Expression, Format, ...) FU_ENSURE_IMPLEMENTATION(&, true, Expression, Format, ##__VA_ARGS__)
+
+#else
+
+#define FU_ENSURE(Expression) (Expression)
+#define FU_ENSURE_MESSAGE(Expression, Format, ...) (Expression)
+#define FU_ENSURE_ALWAYS(Expression) (Expression)
+#define FU_ENSURE_ALWAYS_MESSAGE(Expression, Format, ...) (Expression)
+
+#endif
