708b1b066a98f2c347bd44b9fd704e7308e804db
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2024-11-16 23:45:24 +0800

    New: FRemAlphaBlendCurveAndOption added; static GetAlpha API added

--- Source/RemCommon/Private/RemAlphaBlend.cpp
+++ Source/RemCommon/Private/RemAlphaBlend.cpp
@@ -3,5 +3,45 @@
 
 #include "RemAlphaBlend.h"
 
+#include "AlphaBlend.h"
+#include "Curves/CurveBase.h"
+#include "Curves/RichCurve.h"
+#include "Macro/RemAssertionMacros.h"
+
 #include UE_INLINE_GENERATED_CPP_BY_NAME(RemAlphaBlend)
 
+FVector3f FRemAlphaBlendCurveAndOption::GetAlpha(const float Time, const float BlendTime,
+	const EAlphaBlendOption BlendOption, const FRemCurveBaseWrapper CurveWrapper, bool& bOutVectorAlpha)
+{
+	RemCheckCondition(BlendTime > 0.0f, return FVector3f::OneVector;, REM_NO_LOG_BUT_ENSURE);
+
+	const auto Alpha = FMath::Clamp(Time / BlendTime, 0.0f, 1.0f);
+
+	auto* Curve = CurveWrapper.Get();
+	if (!Curve || BlendOption != EAlphaBlendOption::Custom)
+	{
+		bOutVectorAlpha = false;
+		return FVector3f{FAlphaBlend::AlphaToBlendOption(Alpha, BlendOption)};
+	}
+
+	const auto& Curves = Curve->GetCurves();
+
+	RemCheckCondition(Curves.Num() > 0, return FVector3f::OneVector;, REM_NO_LOG_BUT_ENSURE);
+	bOutVectorAlpha = Curves.Num() >= 3;
+
+	auto VectorAlpha{FVector3f::OneVector};
+	for (const auto Index : {0, 1, 2})
+	{
+		if (Curves.Num() > Index)
+		{
+			VectorAlpha[Index] = Rem::EvaluateCurve01Clamped(*Curves[Index].CurveToEdit, Alpha);
+		}
+		else
+		{
+			// copy value from previous element
+			VectorAlpha[Index] = VectorAlpha[Index - 1];
+		}
+	}
+
+	return VectorAlpha;
+}
--- Source/RemCommon/Public/RemAlphaBlend.h
+++ Source/RemCommon/Public/RemAlphaBlend.h
@@ -2,36 +2,55 @@
 
 #pragma once
 
+#include "RemCommonWrapperTypes.h"
+
 #include "RemAlphaBlend.generated.h"
 
 enum class EAlphaBlendOption : uint8;
 class UCurveBase;
 
 USTRUCT(BlueprintType)
-struct FRemAlphaBlend
+struct REMCOMMON_API FRemAlphaBlend
 {
 	GENERATED_BODY()
 	
-	UPROPERTY(EditAnywhere, Category = "Blend", meta = (ClampMin = "0"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Blend", meta = (ClampMin = "0", ForceUnits = "s"))
 	float BlendTime{1.0f};
 };
 
 USTRUCT(Blueprintable)
-struct FRemAlphaBlendOption : public FRemAlphaBlend
+struct REMCOMMON_API FRemAlphaBlendOption : public FRemAlphaBlend
 {
 	GENERATED_BODY()
 	
 	/** Type of blending used (Linear, Cubic, etc.) */
-	UPROPERTY(EditAnywhere, Category = "Blend")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Blend")
 	EAlphaBlendOption BlendOption{};
 };
 
 USTRUCT(Blueprintable)
-struct FRemAlphaBlendCurve : public FRemAlphaBlend
+struct REMCOMMON_API FRemAlphaBlendCurve : public FRemAlphaBlend
 {
 	GENERATED_BODY()
-	
-	/** Type of blending used (Linear, Cubic, etc.) */
-	UPROPERTY(EditAnywhere, Category = "Blend", meta = (AllowedClasses = "/Script/Engine.CurveFloat, /Script/Engine.CurveVector"))
-	TObjectPtr<UCurveBase> Curve;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Blend")
+	FRemCurveBaseWrapper Curve;
+};
+
+/**
+ * It has the same size as FRemAlphaBlendCurve, but could serve as an all in one alternative where selection by type is not needed
+ */
+USTRUCT(Blueprintable)
+struct REMCOMMON_API FRemAlphaBlendCurveAndOption : public FRemAlphaBlendOption
+{
+	GENERATED_BODY()
+
+	/**
+	 * Curve used when BlendOption set to Custom
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Blend")
+	FRemCurveBaseWrapper Curve;
+
+	static FVector3f GetAlpha(float Time, float BlendTime,
+		EAlphaBlendOption BlendOption, const FRemCurveBaseWrapper CurveWrapper, bool& bOutVectorAlpha);
 };
