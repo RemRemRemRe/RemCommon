9fcceef0132e676b81ccaed403fdbad4de83c828
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2024-12-29 19:52:04 +0800

    Changed: align naming with std

--- Source/RemCommon/Public/RemCppConcepts.h
+++ Source/RemCommon/Public/RemCppConcepts.h
@@ -6,8 +6,6 @@
 
 namespace Rem::Concepts
 {
-
-template <typename T>
-concept is_enum_class = std::is_enum_v<T> && !std::is_convertible_v<T, std::underlying_type_t<T>>;
-
+	template <typename T>
+	concept is_scoped_enum = std::is_enum_v<T> && !std::is_convertible_v<T, std::underlying_type_t<T>>;
 }
