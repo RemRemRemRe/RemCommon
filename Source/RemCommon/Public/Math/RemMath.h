d5ec7ad2f31fd2d8df2a9dcad9d7fe70cfb123d1
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2025-01-16 01:32:47 +0800

    New: import and enhance Interpolate APIs from ALS-Refactored

--- Source/RemCommon/Public/Math/RemMath.h
+++ Source/RemCommon/Public/Math/RemMath.h
@@ -2,10 +2,15 @@
 
 #pragma once
 
+#include <type_traits>
+
 #include "RemAlwaysFalse.h"
+#include "Templates/RemIsInstance.h"
 
 namespace Rem::Math
 {
+	constexpr auto CounterClockwiseRotationAngleThreshold{5.0f};
+
 	template<typename T>
 	requires (!std::is_signed_v<T>)
 	T GetBitsNeeded(const T Value)
@@ -41,12 +46,14 @@ namespace Rem::Math
 
 	/** Clamps Value to be between 0 and 1, inclusive */
 	template< class T >
+	requires std::is_floating_point_v<T>
 	[[nodiscard]] constexpr T Clamp01(const T Value)
 	{
 		return FMath::Clamp<T>(Value, 0.0f, 1.0f);
 	}
 	
 	template< class T >
+	requires std::is_floating_point_v<T>
 	[[nodiscard]] constexpr decltype(auto) Saturate(const T Value)
 	{
 		return Clamp01<T>(Value);
@@ -64,4 +71,137 @@ namespace Rem::Math
 		return UE::Math::TVector2<T>{std::max(Min1, Min2), std::min(Max1, Max2) };
 	}
 
+	template <typename ValueType>
+	requires std::is_floating_point_v<ValueType>
+	constexpr ValueType RemapAngleForCounterClockwiseRotation(const ValueType Angle)
+	{
+		if (Angle > 180.0f - CounterClockwiseRotationAngleThreshold)
+		{
+			return Angle - 360.0f;
+		}
+
+		return Angle;
+	}
+
+	inline float Damp(const float DeltaTime, const float Smoothing)
+	{
+		// https://www.rorydriscoll.com/2016/03/07/frame-rate-independent-damping-using-lerp/
+
+		return 1.0f - FMath::Pow(Smoothing, DeltaTime);
+	}
+
+	inline float ExponentialDecay(const float DeltaTime, const float Lambda)
+	{
+		// https://www.rorydriscoll.com/2016/03/07/frame-rate-independent-damping-using-lerp/
+
+		return 1.0f - FMath::InvExpApprox(Lambda * DeltaTime);
+	}
+
+	template<typename ValueType>
+	requires std::is_floating_point_v<ValueType>
+	struct TAngle
+	{
+		ValueType Angle;
+		explicit constexpr TAngle(const ValueType Angle) : Angle(Angle) {}
+
+		const ValueType& operator*() const
+		{
+			return Angle;
+		}
+
+		ValueType& operator*()
+		{
+			return Angle;
+		}
+	};
+
+	template <typename T>
+	T Interpolate(const T& From, const T& To, float Ratio)
+	{
+		if constexpr (is_instance_v<T, UE::Math::TRotator>)
+		{
+			auto Result{To - From};
+			Result.Normalize();
+
+			Result.Pitch = RemapAngleForCounterClockwiseRotation(Result.Pitch);
+			Result.Yaw = RemapAngleForCounterClockwiseRotation(Result.Yaw);
+			Result.Roll = RemapAngleForCounterClockwiseRotation(Result.Roll);
+
+			Result *= Ratio;
+			Result += From;
+			Result.Normalize();
+
+			return Result;
+		}
+		else if constexpr (is_instance_v<T, TAngle>)
+		{
+			auto Delta{FMath::UnwindDegrees(*To - *From)};
+			Delta = RemapAngleForCounterClockwiseRotation(*Delta);
+
+			return FMath::UnwindDegrees(*From + *Delta * Ratio);
+		}
+		else
+		{
+			return FMath::Lerp(From, To, Ratio);
+		}
+	}
+
+	template <typename T>
+	T ExponentialDecay(const T& Current, const T& Target, const float DeltaTime, const float Lambda)
+	{
+		if (Lambda <= 0.0f)
+		{
+			return Target;
+		}
+
+		return Interpolate(Current, Target, ExponentialDecay(DeltaTime, Lambda));
+	}
+
+	template <typename T>
+	T Damp(const T& Current, const T& Target, const float DeltaTime, const float Lambda)
+	{
+		if (Lambda <= 0.0f)
+		{
+			return Target;
+		}
+
+		return Interpolate(Current, Target, Damp(DeltaTime, Lambda));
+	}
+
+	template <typename ValueType, typename StateType>
+	ValueType SpringDamp(StateType& SpringState, const ValueType& Current, const ValueType& Target, const float DeltaTime,
+		const float Frequency, const float DampingRatio, const float TargetVelocityAmount)
+	{
+		if (DeltaTime <= UE_SMALL_NUMBER)
+		{
+			return Current;
+		}
+
+		if (!SpringState.bStateValid)
+		{
+			SpringState.Velocity = ValueType{0.0f};
+			SpringState.PreviousTarget = Target;
+			SpringState.bStateValid = true;
+
+			return Target;
+		}
+
+		ValueType Result{Current};
+		FMath::SpringDamper(Result, SpringState.Velocity, Target,
+							(Target - SpringState.PreviousTarget) * Clamp01(TargetVelocityAmount) / DeltaTime,
+							DeltaTime, Frequency, DampingRatio);
+
+		SpringState.PreviousTarget = Target;
+
+		return Result;
+	}
+
+	/**
+	 * Combine 2 rotations to give you the resulting rotation of first applying A, then B
+	 */
+	template <typename T>
+	[[nodiscard]] auto CombineRotator(const UE::Math::TRotator<T>& Left, const UE::Math::TRotator<T>& Right) -> auto
+	{
+		return UE::Math::TRotator<T>{Right.Quaternion() * Left.Quaternion()};
+	}
 }
