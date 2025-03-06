a78f842e14ca144307bd54bd32cb23cbdb041225
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2025-03-07 01:12:23 +0800

    Fixed: pretty function name on Linux

--- Source/RemCommon/Public/Macro/RemMacroUtilities.h
+++ Source/RemCommon/Public/Macro/RemMacroUtilities.h
@@ -9,9 +9,19 @@
 #define REM_INITIALIZE_NET_DEBUG_STRING(Object, VariableName) \
 	auto VariableName = Rem::GetNetDebugString(Object);
 
+#if defined(__clang__) || defined(__GNUC__)
+
+#define REM_INITIALIZE_SOURCE_LOCATION_STRING(VariableName) \
+	auto VariableName = FString::Format(TEXT("[Function: {0}] [Line: {1}]"), \
+	{__PRETTY_FUNCTION__, __LINE__});
+
+#else
+
 #define REM_INITIALIZE_SOURCE_LOCATION_STRING(VariableName) \
 	auto VariableName = FString::Format(TEXT("[Function: {0}] [Line: {1}]"), \
 	{__FUNCTION__, __LINE__});
+
+#endif
 
 #ifdef REM_WITH_DEVELOPMENT_ONLY_CODE
 
