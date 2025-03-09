42a24d4e0b48fd4c1ae94f82e968518f5675524e
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2025-03-09 21:39:56 +0800

    Changed: rename

--- Source/RemCommon/Private/Interface/RemStringInterface.cpp
+++ Source/RemCommon/Private/Interface/RemStringInterface.cpp
@@ -4,12 +4,3 @@
 #include "Interface/RemStringInterface.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(RemStringInterface)
-
-
-FString IStringInterface::ToString() const
-{
-	// if you don't want to print in some case like shipping,
-	// could change this behavior here
-	return ToStringInternal();
-}
-
--- Source/RemCommon/Public/Interface/RemStringInterface.h
+++ Source/RemCommon/Public/Interface/RemStringInterface.h
@@ -3,26 +3,25 @@
 #pragma once
 
 #include "UObject/Interface.h"
+
 #include "Macro/RemCoreMiscDefines.h"
 #include "RemCommonLog.h"
+
 #include "RemStringInterface.generated.h"
 
-/** Interface for printing a object in string format */
-UINTERFACE(MinimalAPI, NotBlueprintable)
-class UStringInterface : public UInterface
+UINTERFACE(MinimalAPI)
+class URemStringInterface : public UInterface
 {
 	GENERATED_BODY()
 };
 
-class REMCOMMON_API IStringInterface
+/** Interface for printing a object in string format */
+class REMCOMMON_API IRemStringInterface
 {
 	GENERATED_BODY()
 
 public:
-	FString ToString() const;
-
-protected:
-	virtual FString ToStringInternal() const
+	virtual FString ToString() const
 		REM_VIRTUAL_WARN(LogRemCommon, return {};);
-	
+
 };
