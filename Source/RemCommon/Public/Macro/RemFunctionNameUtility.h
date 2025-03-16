266a2d03473001a4f842a005fb4eed3335973c4e
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2025-03-16 23:25:48 +0800

    Fixed: pretty function name on Linux, parameter list stripped

---
+++ Source/RemCommon/Public/Macro/RemFunctionNameUtility.h
@@ -0,0 +1,74 @@
+// Copyright RemRemRemRe. 2025. All Rights Reserved.
+
+#pragma once
+
+
+#include <string_view>
+
+#define REM_FUNCTION_SIGNATURE_HELPER() REM_FUNCTION_SIGNATURE
+#define REM_FUNCTION_NAME_VIEW \
+	( \
+		[]() constexpr noexcept -> std::string_view \
+		{ \
+			return Rem::Private::TFunctionNameParser<__COUNTER__>::ParseSignature(REM_FUNCTION_SIGNATURE_HELPER()); \
+		}() \
+	)
+
+#if defined(__GNUC__) || defined(__clang__)
+
+#define REM_FUNCTION_SIGNATURE __PRETTY_FUNCTION__
+#define REM_FUNCTION_NAME \
+	[]() \
+	{ \
+		auto View = REM_FUNCTION_NAME_VIEW; \
+		return FString::ConstructFromPtrSize(View.data(), View.size()); \
+	}()
+
+#elif defined(_MSC_VER)
+
+#define REM_FUNCTION_SIGNATURE __FUNCSIG__
+#define REM_FUNCTION_NAME __FUNCTION__
+
+#else
+
+#define REM_FUNCTION_SIGNATURE __FUNCTION__
+#define REM_FUNCTION_NAME __FUNCTION__
+
+#endif
+
+namespace Rem::Private
+{
+	template <size_t CompileTimeCounter>
+	struct TFunctionNameParser
+	{
+		constexpr static std::string_view ParseSignature(const char* FunctionSignatureRaw) noexcept
+		{
+			std::string_view FunctionSignature = FunctionSignatureRaw;
+
+			const auto LeftParenthesisPosition = [&]{
+				size_t Position = FunctionSignature.find('(');
+				return Position != std::string_view::npos ? Position : FunctionSignature.size();
+			}();
+
+			std::string_view FunctionSignatureNoArgs = FunctionSignature.substr(0, LeftParenthesisPosition);
+
+			const auto PositionOfLastSpace = [&]{
+				const size_t Position = FunctionSignatureNoArgs.rfind(' ');
+
+#ifdef _MSC_VER
+	 // ReSharper disable once CommentTypo
+				// MSVC specific things like " __cdecl"
+				if (Position == std::string_view::npos) {
+					Position = FunctionSignatureNoArgs.rfind('>'); // template specialization
+					if (Position != std::string_view::npos) Position += 2;
+				}
+#endif
+
+				return Position != std::string_view::npos ? Position + 1 : 0;
+			}();
+
+			const auto Result = FunctionSignatureNoArgs.substr(PositionOfLastSpace);
+			return Result;
+		}
+	};
+}
--- Source/RemCommon/Public/Macro/RemMacroUtilities.h
+++ Source/RemCommon/Public/Macro/RemMacroUtilities.h
@@ -3,25 +3,16 @@
 #pragma once
 
 #include "RemMisc.h"
+#include "RemFunctionNameUtility.h"
 
 #define REM_WITH_DEVELOPMENT_ONLY_CODE !UE_BUILD_SHIPPING
 
 #define REM_INITIALIZE_NET_DEBUG_STRING(Object, VariableName) \
 	auto VariableName = Rem::GetNetDebugString(Object);
 
-#if defined(__clang__) || defined(__GNUC__)
-
 #define REM_INITIALIZE_SOURCE_LOCATION_STRING(VariableName) \
 	auto VariableName = FString::Format(TEXT("[Function: {0}] [Line: {1}]"), \
-	{__PRETTY_FUNCTION__, __LINE__});
-
-#else
-
-#define REM_INITIALIZE_SOURCE_LOCATION_STRING(VariableName) \
-	auto VariableName = FString::Format(TEXT("[Function: {0}] [Line: {1}]"), \
-	{__FUNCTION__, __LINE__});
-
-#endif
+	{REM_FUNCTION_NAME, __LINE__});
 
 #ifdef REM_WITH_DEVELOPMENT_ONLY_CODE
 
