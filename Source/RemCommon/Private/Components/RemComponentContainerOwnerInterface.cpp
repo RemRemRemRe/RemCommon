6f8ed1ae40addb4fe7bd024762deba515615813e
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2025-04-20 23:45:07 +0800

    Changed: implementation of GetComponentContainerView is now optional

--- Source/RemCommon/Private/Components/RemComponentContainerOwnerInterface.cpp
+++ Source/RemCommon/Private/Components/RemComponentContainerOwnerInterface.cpp
@@ -3,4 +3,17 @@
 
 #include "Components/RemComponentContainerOwnerInterface.h"
 
+#include "StructUtils/StructView.h"
+#include "Components/RemComponentContainer.h"
+
 #include UE_INLINE_GENERATED_CPP_BY_NAME(RemComponentContainerOwnerInterface)
+
+TConstStructView<FRemComponentContainer> IRemComponentContainerOwnerInterface::GetComponentContainerView() const
+{
+	return GetComponentContainer();
+}
+
+TStructView<FRemComponentContainer> IRemComponentContainerOwnerInterface::GetComponentContainerView()
+{
+	return GetComponentContainer();
+}
--- Source/RemCommon/Public/Components/RemComponentContainerOwnerInterface.h
+++ Source/RemCommon/Public/Components/RemComponentContainerOwnerInterface.h
@@ -18,13 +18,17 @@ class URemComponentContainerOwnerInterface : public UInterface
 	GENERATED_BODY()
 };
 
+/**
+ * @note the default implementation for GetComponentContainerView is not sufficient,
+ * if you returned derived type of FRemComponentContainer, GIVE YOUR OWN implementation in the case
+ */
 class REMCOMMON_API IRemComponentContainerOwnerInterface
 {
 	GENERATED_BODY()
 
 public:
-	virtual TConstStructView<FRemComponentContainer> GetComponentContainerView() const = 0;
-	virtual TStructView<FRemComponentContainer> GetComponentContainerView() = 0;
+	virtual TConstStructView<FRemComponentContainer> GetComponentContainerView() const;
+	virtual TStructView<FRemComponentContainer> GetComponentContainerView();
 
 	virtual FRemComponentContainer& GetComponentContainer() = 0;
 	virtual const FRemComponentContainer& GetComponentContainer() const = 0;
