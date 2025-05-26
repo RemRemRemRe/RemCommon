0cc2a8ae9a593056fab0ff0aed60dd604789e610
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2025-05-27 00:42:00 +0800

    New: macro REM_SET_TRUE_IF, REM_SET_FALSE_IF

--- Source/RemCommon/Public/Macro/RemMacroUtilities.h
+++ Source/RemCommon/Public/Macro/RemMacroUtilities.h
@@ -27,3 +27,16 @@
 #define REM_RETURN_IF_NOT_GAME_WORLD
 
 #endif
+
+#define REM_IF_THEN(Condition, Statement) \
+	do \
+	{ \
+		if (Condition) \
+		{ \
+			Statement \
+		}\
+	}\
+	while (false)
+
+#define REM_SET_TRUE_IF(Variable) REM_IF_THEN(Variable, Variable = true;)
+#define REM_SET_FALSE_IF(Variable) REM_IF_THEN(Variable, Variable = false;)
