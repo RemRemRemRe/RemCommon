07e0928643b98b84e4e23ae3f78149df0a84cc0f
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2025-06-17 00:51:13 +0800

    New: some other concepts to workaround comma issue in parameter list of a macro

---
+++ Source/RemCommon/Public/RemHelperConcepts.h
@@ -0,0 +1,19 @@
+﻿// Copyright RemRemRemRe. 2025. All Rights Reserved.
+
+#pragma once
+
+#include <concepts>
+
+class UStateTreeComponent;
+class UAbilitySystemComponent;
+
+namespace Rem::Concepts
+{
+
+template<class T>
+concept is_ability_system_component = std::derived_from<T, UAbilitySystemComponent>;
+
+template<class T>
+concept is_statetree_component = std::derived_from<T, UStateTreeComponent>;
+
+}
