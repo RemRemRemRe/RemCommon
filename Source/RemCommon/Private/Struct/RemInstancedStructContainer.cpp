c95839b02c0a799bee1d237241477a4c519ecf25
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2025-11-01 02:02:21 +0800

    New: FRemInstancedStructContainer
    add an InstancedStructContainer with optimized layout and a map for quick indexing

---
+++ Source/RemCommon/Private/Struct/RemInstancedStructContainer.cpp
@@ -0,0 +1,84 @@
+﻿// Copyright RemRemRemRe. 2025. All Rights Reserved.
+
+#include "Struct/RemInstancedStructContainer.h"
+
+#include "RemBinarySearch.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(RemInstancedStructContainer)
+
+FStructView FRemInstancedStructContainer::TryGetView(const FKeyType Key)
+{
+    auto* Index = IndexMap.Find(Key);
+    RemEnsureVariable(Index, return {}, REM_NO_LOG_OR_ASSERTION);
+    
+    return StructContainer[*Index];
+}
+
+FConstStructView FRemInstancedStructContainer::TryGetView(const FKeyType Key) const
+{
+    auto* Index = IndexMap.Find(Key);
+    RemEnsureVariable(Index, return {}, REM_NO_LOG_OR_ASSERTION);
+    
+    return StructContainer[*Index];
+}
+
+bool FRemInstancedStructContainer::Contains(const FKeyType Key) const
+{
+    return IndexMap.Contains(Key);
+}
+
+bool FRemInstancedStructContainer::TryAdd(const FKeyType Key, const FConstStructView NewStruct)
+{
+    if (Contains(Key))
+    {
+        return false;
+    }
+
+    // StructMemory is optional
+    RemEnsureVariable(NewStruct.GetScriptStruct(), return false, REM_NO_LOG_OR_ASSERTION);
+
+    const auto IndexToInsert = Rem::Algo::UpperBound(StructContainer,
+        NewStruct.GetScriptStruct()->GetMinAlignment(),
+        [](const FConstStructView View)
+        {                    
+            return View.GetScriptStruct()->GetMinAlignment();
+        });
+
+    if (IndexToInsert == StructContainer.Num())
+    {
+        // it has the greatest alignment
+        StructContainer.Append(MakeConstArrayView(&NewStruct, 1));
+    }
+    else
+    {
+        // first alignment greater than it is found
+
+        // increment existed indexes
+        for (auto& IndexPair : IndexMap)
+        {
+            if (IndexPair.Value >= IndexToInsert)
+            {
+                ++IndexPair.Value;
+            }
+        }
+
+        // insert new one
+        StructContainer.InsertAt(IndexToInsert, MakeConstArrayView(&NewStruct, 1));
+    }
+                
+    IndexMap.Add(Key, IndexToInsert);
+
+    return true;
+}
+
+void FRemInstancedStructContainer::Reset()
+{
+    StructContainer.Reset();
+    IndexMap.Reset();
+}
+
+void FRemInstancedStructContainer::Empty()
+{
+    StructContainer.Empty();
+    IndexMap.Empty();
+}
--- Source/RemCommon/Public/Struct/RemInstancedStructContainer.h
+++ Source/RemCommon/Public/Struct/RemInstancedStructContainer.h
@@ -2,9 +2,64 @@
 
 #pragma once
 
+#include "Macro/RemAssertionMacros.h"
 #include "StructUtils/InstancedStructContainer.h"
+
+#include "RemInstancedStructContainer.generated.h"
 
 [[nodiscard]] inline int32 GetNum(const FInstancedStructContainer& Container)
 {
     return Container.Num();
 }
+
+#define REM_API REMCOMMON_API
+
+/**
+ * an InstancedStructContainer with optimized layout and a map for quick indexing
+ *
+ * @note currently, structs would be stored in the oder of their MinAlignment for more compact layout
+ */
+USTRUCT(BlueprintType)
+struct FRemInstancedStructContainer
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, Category = "Rem")
+    FInstancedStructContainer StructContainer{};
+    
+    UPROPERTY(EditAnywhere, Category = "Rem")
+    TMap<uint32, int32> IndexMap{};
+    
+    using FKeyType = uint32;
+
+    REM_API FStructView TryGetView(const FKeyType Key);
+    REM_API FConstStructView TryGetView(const FKeyType Key) const;
+    
+    template <typename T>
+    T* TryGetPtr(const FKeyType Key) const
+    {
+        const auto View = TryGetView(Key);
+        auto* Ptr = View.GetPtr<const T>();
+        return const_cast<T*>(Ptr);
+    }
+
+    template <typename T>
+    T& Get(const FKeyType Key) const
+    {
+        const auto View = TryGetView(Key);
+        auto& Ref = View.Get<const T>();
+        return const_cast<T&>(Ref);
+    }
+
+    REM_API bool Contains(FKeyType Key) const;
+    
+    /**
+     * add only if it didn't exist before
+     */
+    REM_API bool TryAdd(FKeyType Key, FConstStructView NewStruct);
+    
+    REM_API void Reset();
+    REM_API void Empty();
+};
+
+#undef REM_API
