a6e0a85b9e79499ca912ffa2eb0e5a27d7c28a96
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2026-03-23 00:53:54 +0800

    New: blueprint enum type ERemTransitionType which basically mirrors ETransitionType

---
+++ Source/RemCommon/Private/Enum/RemTransitionType.cpp
@@ -0,0 +1,6 @@
+﻿// Copyright RemRemRemRe. 2026. All Rights Reserved.
+
+
+#include "Enum/RemTransitionType.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(RemTransitionType)
---
+++ Source/RemCommon/Public/Enum/RemTransitionType.h
@@ -0,0 +1,94 @@
+﻿// Copyright RemRemRemRe. 2026. All Rights Reserved.
+
+#pragma once
+
+#include "RemEnumStatics.inl"
+
+#include "RemTransitionType.generated.h"
+
+namespace Rem::Enum
+{
+    enum class ETransitionType : uint8;
+
+    UENUM(BlueprintType)
+    enum class ERemTransitionType : uint8
+    {
+        Abort,
+        ToNext,
+        ToPrevious,
+        ToStart,
+        ToEnd,
+        Restart,
+    };
+    
+    template<ERemTransitionType Transition, typename T>
+    requires std::is_signed_v<T>
+    [[nodiscard]] constexpr const TTuple<ETransitionType, T>& EnumToTransition()
+    {
+        if constexpr (Transition == ERemTransitionType::Abort)
+        {
+            return Transition::Abort<T>;
+        }
+        else if constexpr (Transition == ERemTransitionType::ToNext)
+        {
+            return Transition::ToNext<T>;
+        }
+        else if constexpr (Transition == ERemTransitionType::ToPrevious)
+        {
+            return Transition::ToPrevious<T>;
+        }
+        else if constexpr (Transition == ERemTransitionType::ToStart)
+        {
+            return Transition::ToStart<T>;
+        }
+        else if constexpr (Transition == ERemTransitionType::ToEnd)
+        {
+            return Transition::ToEnd<T>;
+        }
+        else if constexpr (Transition == ERemTransitionType::Restart)
+        {
+            return Transition::Restart<T>;
+        }
+        else
+        {
+            static_assert(Rem::always_false<T>::value, "Unsupported Transition Type");
+            return Transition::Abort<T>;
+        }
+    }
+    
+    template<typename T>
+    requires std::is_signed_v<T>
+    [[nodiscard]] constexpr const TTuple<ETransitionType, T>& EnumToTransition(const ERemTransitionType Transition)
+    {
+        if (Transition == ERemTransitionType::Abort)
+        {
+            return EnumToTransition<ERemTransitionType::Abort, T>();
+        }
+        if (Transition == ERemTransitionType::ToNext)
+        {
+            return EnumToTransition<ERemTransitionType::ToNext, T>();
+        }
+        if (Transition == ERemTransitionType::ToPrevious)
+        {
+            return EnumToTransition<ERemTransitionType::ToPrevious, T>();
+        }
+        if (Transition == ERemTransitionType::ToStart)
+        {
+            return EnumToTransition<ERemTransitionType::ToStart, T>();
+        }
+        if (Transition == ERemTransitionType::ToEnd)
+        {
+            return EnumToTransition<ERemTransitionType::ToEnd, T>();
+        }
+        // if (Transition == ERemTransitionType::Restart)
+        {
+#if REM_WITH_DEVELOPMENT_ONLY_CODE
+            
+            RemCheckCondition(Transition == ERemTransitionType::Restart);
+            
+#endif
+            
+            return EnumToTransition<ERemTransitionType::Restart, T>();
+        }
+    }
+}
