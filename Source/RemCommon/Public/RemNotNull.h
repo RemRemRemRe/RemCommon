85994a7f52ad69c60b98f5c489ae8f79c3b87e9c
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2026-04-05 21:21:51 +0800

    New: introduce TNotNull and MakeNotNull

---
+++ Source/RemCommon/Public/RemNotNull.h
@@ -0,0 +1,33 @@
+﻿// Copyright RemRemRemRe. 2026. All Rights Reserved.
+
+#pragma once
+
+#include "Misc/NotNull.h"
+#include "Templates/RemIsInstance.h"
+#include "Macro/RemFunctorMacro.h"
+
+namespace Rem
+{
+    template <typename T>
+    using TNotNull = ::TNotNull<T>;
+    
+    template<typename T>
+    decltype(auto) MakeNotNull(T&& Object)
+    {
+        using Type = std::remove_reference_t<T>;
+        if constexpr (std::is_pointer_v<Type>)
+        {
+            return TNotNull<Type>{ Object };
+        }
+        else if constexpr (TIsTObjectPtr_V<Type>)
+        {
+            return TNotNull<typename Type::ElementType*>{ Object.Get() };
+        }
+        else
+        {
+            return TNotNull<Type*>{ &Object };
+        }
+    }
+    
+    REM_FUNCTION_TO_FUNCTOR_SIMPLE(MakeNotNull)
+}
