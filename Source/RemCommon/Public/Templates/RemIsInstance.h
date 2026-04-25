7043d3a835ddf7b5919c082d97094071b284a980
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2026-04-25 15:54:20 +0800

    Misc: spacing

--- Source/RemCommon/Public/Templates/RemIsInstance.h
+++ Source/RemCommon/Public/Templates/RemIsInstance.h
@@ -6,8 +6,10 @@
 
 namespace Rem
 {
+
 namespace Private
 {
+
 template <typename, template <typename...> typename>
 struct TIsInstanceImpl : std::false_type
 {
@@ -17,6 +19,7 @@ template <template <typename...> typename U, typename... Ts>
 struct TIsInstanceImpl<U<Ts...>, U> : std::true_type
 {
 };
+
 }
 
 // check if type T is an instantiation of template U
@@ -26,4 +29,5 @@ using is_instance = Private::TIsInstanceImpl<std::remove_cvref_t<T>, U>;
 
 template <typename T, template <typename...> typename U>
 constexpr bool is_instance_v = Private::TIsInstanceImpl<std::remove_cvref_t<T>, U>::value;
+
 }
