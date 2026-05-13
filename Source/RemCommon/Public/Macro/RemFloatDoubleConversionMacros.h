8e1858619f7f04ee6a3ad4841880f6ba64c75fc1
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2026-05-14 01:56:04 +0800

    New: add REM_ROTATOR macro

--- Source/RemCommon/Public/Macro/RemFloatDoubleConversionMacros.h
+++ Source/RemCommon/Public/Macro/RemFloatDoubleConversionMacros.h
@@ -7,12 +7,14 @@
 #if REM_ENABLE_FLOAT_DOUBLE_CONVERSION_MACRO
 
 #define REM_QUAT_DOUBLE(...) FQuat4d{ __VA_ARGS__ }
+#define REM_ROTATOR_DOUBLE(...) FRotator3d{ __VA_ARGS__ }
 
 #define REM_VECTOR2_DOUBLE(...) FVector2d{ __VA_ARGS__ }
 #define REM_VECTOR3_DOUBLE(...) FVector3d{ __VA_ARGS__ }
 #define REM_VECTOR4_DOUBLE(...) FVecto4d{ __VA_ARGS__ }
 
 #define REM_QUAT_FLOAT(...) FQuat4f{ __VA_ARGS__ }
+#define REM_ROTATOR_FLOAT(...) FRotator3f{ __VA_ARGS__ }
 
 #define REM_VECTOR2_FLOAT(...) FVector2f{ __VA_ARGS__ }
 #define REM_VECTOR3_FLOAT(...) FVector3f{ __VA_ARGS__ }
@@ -21,12 +23,14 @@
 #else
 
 #define REM_DOUBLE_QUAT(...) __VA_ARGS__
+#define REM_ROTATOR_DOUBLE(...) FRotator3d{ __VA_ARGS__ }
 
 #define REM_DOUBLE_VECTOR2(...) __VA_ARGS__
 #define REM_DOUBLE_VECTOR3(...) __VA_ARGS__
 #define REM_DOUBLE_VECTOR4(...) __VA_ARGS__
 
 #define REM_FLOAT_QUAT(...) __VA_ARGS__
+#define REM_ROTATOR_FLOAT(...) FRotator3f{ __VA_ARGS__ }
 
 #define REM_FLOAT_VECTOR2(...) __VA_ARGS__
 #define REM_FLOAT_VECTOR3(...) __VA_ARGS__
