d47925abbb6c83e5b4d5e165f9227570ab4364e3
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2026-07-22 00:12:39 +0800

    New: API ToTagContainer for FRemGameplayTagArray

--- Source/RemCommon/Public/GameplayTag/RemGameplayTagArray.h
+++ Source/RemCommon/Public/GameplayTag/RemGameplayTagArray.h
@@ -22,5 +22,14 @@ struct REMCOMMON_API FRemGameplayTagArray
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rem", meta = (EditInline, TitleProperty = TagName))
     TArray<FGameplayTag> Tags;
 
+    template<class AllocatorType>
+    FGameplayTagContainer ToTagContainer() const;
+
     REM_DEFINE_GETTERS_RETURN_REFERENCE(/*no predicate*/, /*no suffix*/, Tags)
 };
+
+template <class TAllocatorType>
+FGameplayTagContainer FRemGameplayTagArray::ToTagContainer() const
+{
+    return FGameplayTagContainer::CreateFromArray<TAllocatorType>(Tags);
+}
