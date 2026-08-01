4e42850a070023c0a20b685811bd176077043b96
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2026-08-01 16:00:18 +0800

    Fixed: guard CNotNull/CNotNullOf concepts with UE_ENABLE_NOTNULL_WRAPPER

--- Source/RemCommon/Public/RemNotNull.h
+++ Source/RemCommon/Public/RemNotNull.h
@@ -10,6 +10,8 @@ namespace Rem
 template <typename T>
 using TNotNull = TNotNull<T>;
 
+#if UE_ENABLE_NOTNULL_WRAPPER
+
 template <typename T>
 concept CNotNull = UE::Core::Private::TIsTNotNullParam_V<T>;
 
@@ -17,6 +19,17 @@ template <typename NotNull, typename ValueTypeBase>
 concept CNotNullOf = CNotNull<NotNull>
                      && std::derived_from<std::remove_cvref_t<std::remove_pointer_t<decltype(*std::declval<
                                               NotNull>())>>, ValueTypeBase>;
+
+#else // !UE_ENABLE_NOTNULL_WRAPPER
+
+// Wrapper disabled: TNotNull<T> aliases T, so nothing satisfies the not-null concept.
+template <typename T>
+concept CNotNull = false;
+
+template <typename NotNull, typename ValueTypeBase>
+concept CNotNullOf = false;
+
+#endif // UE_ENABLE_NOTNULL_WRAPPER
 
 /**
  * pointer can convert / construct TNotNull implicitly, but there are cases to copy const from T
